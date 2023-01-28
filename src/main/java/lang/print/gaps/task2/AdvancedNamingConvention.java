d02b2d5ccb4f2d163bddc5769c17bcaf3fc63c04
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-01-28 18:54:19 +0400

    Create  AdvancedNamingConvention.java

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,18 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+     public static void main(String[] args){}
+        
+        private static final int ADULT_AGE = 18;
+
+        private int age;
+        private int phoneNumber;
+
+        void callToFriend() {
+            //some code
+        }
+
+        void callByNumber(int number) {
+            //some code
+        }
+    }

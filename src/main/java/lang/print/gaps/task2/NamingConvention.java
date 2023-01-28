9182757e5ef9a423030a6ca449c28193efa38f38
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-01-28 18:57:05 +0400

    Update NamingConvention.java

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,10 +2,10 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
    public static void main(String[] args) {
-        int variableTen = 10;
-        int variableOne = 1;
+        int variable = 10;
+        int variable_one = 1;
         int variableTwo = 2;
-        int variableThree = 3;
+        int variable_three = 3;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_fIVE = 5;
         int VariableSix = 6;
